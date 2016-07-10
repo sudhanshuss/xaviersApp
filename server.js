0fea44ab22bb3466d2685971748710ef70bbac43
sudhanshuss <sudhanshu.shekharr@gmail.com>
2016-07-10 09:52:35 -0700

    modified database

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post('/student/register',middleware.requireAuthentication, function(req, res
 	});
 });
 
-db.sequelize.sync({
+db.sequelize.sync({force:true
 }).then(function() {
 	http.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
